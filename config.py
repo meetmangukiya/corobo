eb07e3507c7c6a2b67dcc8d567fd3c9092ec3457
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-06-08 21:40:20 +0200

    Create config.py

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ import logging
 import os
 
 COBOT_ROOT = os.environ.get('COBOT_ROOT', os.getcwd())
-
+  
 BACKEND = 'Gitter'
-
+ 
 BOT_EXTRA_BACKEND_DIR = os.path.join(COBOT_ROOT, 'err-backend-gitter')
-
+ 
 BOT_DATA_DIR = os.path.join(COBOT_ROOT, 'data')
 BOT_EXTRA_PLUGIN_DIR = os.path.join(COBOT_ROOT, 'plugins')
 
-BOT_LOG_FILE = os.path.join(COBOT_ROOT, 'errbot.log')
-BOT_LOG_LEVEL = logging.DEBUG
+BOT_LOG_FILE = os.path.join(COBOT_ROOT, errbot.log')
+BOT_LOG_LEVEL = logging.DEBUG  
 
 BOT_ADMINS = ('meetmangukiya', )
 
