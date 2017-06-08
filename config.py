32854e3988d79b8534bb7cb23198fe38d70b8076
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-06-08 22:51:39 +0200

    Create config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ COBOT_ROOT = os.environ.get('COBOT_ROOT', os.getcwd())
 
 BACKEND = 'Gitter'
  
+ 
 BOT_EXTRA_BACKEND_DIR = os.path.join(COBOT_ROOT, 'err-backend-gitter')
  
 BOT_DATA_DIR = os.path.join(COBOT_ROOT, 'data')
